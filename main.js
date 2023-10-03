230d103e03b710aca96e1ad9c1d12c6e43ecf774
kettlyncabral <105360394+kettlyncabral@users.noreply.github.com>
2023-10-03 11:16:38 -0300

    okok

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-<section class="teclado">
-         <button class="tecla tecla_pom">Pom</button>
-
-         <button class="tecla tecla_clap">Clap</button>
-         <button class="tecla tecla_tim">Tim</button>
-
-         <button class="tecla tecla_puff">Puff</button>
-         <button class="tecla tecla_splash">Splash</button>
-
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/estilos.css">
-
-<link rel="stylesheet" href="css/estilos.css">
-<script src="main.js"></script>
-</head>
+document.querySelector('#som_tecla_pom').play();
