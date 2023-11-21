b9baea9e13b42f86d0381c07be23231f86a23d9e
kettlyncabral <105360394+kettlyncabral@users.noreply.github.com>
2023-11-21 11:14:38 -0300

    ok

--- main.js
+++ main.js
@@ -16,11 +16,10 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio);
  }
   tecla.onkeydown = function(evento){
-    if(evento.code != 'Tab'){
+    if(evento.code === 'Space' && evento.code === 'Enter'){
       tecla.classList.add('ativa')  
     }
-    
- }
+}
   tecla.onkeyup = function(){
     tecla.classList.remove('ativa');
     }
